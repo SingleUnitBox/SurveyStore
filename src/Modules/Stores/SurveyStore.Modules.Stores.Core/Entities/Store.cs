30428536dcf818fc46cbb853154879ad86a445f7
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-02-29 23:00:05 +0000

    add StoresModule, Store

---
+++ src/Modules/Stores/SurveyStore.Modules.Stores.Core/Entities/Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Modules.Stores.Core.Entities
+{
+    public class Store
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime OpeningTime { get; set; }
+        public DateTime ClosingTime { get; set; }
+    }
+}
