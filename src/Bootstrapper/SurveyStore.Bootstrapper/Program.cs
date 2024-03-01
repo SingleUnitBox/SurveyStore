c9a22576078911967bb9032fa28b55b39cee9caf
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-01 08:07:05 +0000

    add ErrorHandlerMiddleware

--- src/Bootstrapper/SurveyStore.Bootstrapper/Program.cs
+++ src/Bootstrapper/SurveyStore.Bootstrapper/Program.cs
@@ -1,26 +1,15 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace SurveyStore.Bootstrapper
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
+        public static Task Main(string[] args)
+            => CreateHostBuilder(args).Build().RunAsync();
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
---
+++ src/Shared/SurveyStore.Shared.Infrastructure/Exceptions/ErrorHandlerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Infrastructure.Exceptions
+{
+    internal class ErrorHandlerMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+        public ErrorHandlerMiddleware(ILogger<ErrorHandlerMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, exception.Message);
+                await HandleExceptionAsync(exception, context);
+            }
+        }
+
+        private async Task HandleExceptionAsync(Exception exception, HttpContext context)
+        {
+
+        }
+    }
+}
