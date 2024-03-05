5587480bd22ee09072e8339c93fe0abac2dca15c
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-05 21:40:55 +0000

    add AddAuth Extension

---
+++ src/Shared/SurveyStore.Shared.Infrastructure/Auth/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyStore.Shared.Abstractions.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Infrastructure.Auth
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddAuth(this IServiceCollection services, IList<IModule> modules,
+            Action<JwtBearerOptions> optionsFactory = null)
+        {
+            var options = services.GetOptions<AuthOptions>("auth");
+        }
+    }
+}
