e85c7e1018b19128ab62052d15eb638bb12c8b00
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-09 21:13:32 +0000

    add EquipmentController

---
+++ src/Modules/Equipment/SurveyStore.Modules.Equipment.Api/Controllers/EquipmentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SurveyStore.Modules.Equipment.Application.Commands;
+using SurveyStore.Shared.Abstractions.Commands;
+
+namespace SurveyStore.Modules.Equipment.Api.Controllers
+{
+    public class EquipmentController : BaseController
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+
+        public EquipmentController(ICommandDispatcher commandDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPost]
+        public Task<ActionResult> AddTotalStationAsync(AddTotalStation command)
+            => CreatedAtAction(nameof(Get), new { Id = command.Id }, null);
+    }
+}
