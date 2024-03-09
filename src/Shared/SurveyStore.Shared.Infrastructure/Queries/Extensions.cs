012aadfe009b179de334b835c3b1b1284580e2ef
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-09 21:45:23 +0000

    add QueryHandler

---
+++ src/Shared/SurveyStore.Shared.Infrastructure/Queries/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyStore.Shared.Abstractions.Queries;
+
+namespace SurveyStore.Shared.Infrastructure.Queries
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddQueries(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+        {
+            services.AddSingleton<IQueryDispatcher, QueryDispatcher>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Shared/SurveyStore.Shared.Infrastructure/Queries/QueryDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyStore.Shared.Abstractions.Queries;
+
+namespace SurveyStore.Shared.Infrastructure.Queries
+{
+    public class QueryDispatcher : IQueryDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public QueryDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task<TResult> QueryAsync<TQuery, TResult>(TQuery query) where TQuery : class, IQuery
+        {
+            using var scope = _serviceProvider.CreateScope();
+            {
+                var queryHandlerType = typeof(IQueryHandler<,>)
+                    .MakeGenericType(query.GetType(), typeof(TResult));
+                var queryHandler = scope.ServiceProvider.GetRequiredService(queryHandlerType);
+
+                return await (Task<TResult>)queryHandlerType
+                    .GetMethod(nameof(IQueryHandler<IQuery<TResult>, TResult>.HandleAsync))
+                    ?.Invoke(queryHandler, new[] { query });
+            }
+        }
+    }
+}
