9c2ebd6d005c1618f4fc8d360d9e8e30011d3604
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-13 18:59:13 +0000

    add InMemoryMessageBroker

---
+++ src/Shared/SurveyStore.Shared.Infrastructure/Messaging/InMemoryMessageBroker.cs
@@ -0,0 +1,40 @@
+﻿using SurveyStore.Shared.Abstractions.Messaging;
+using SurveyStore.Shared.Abstractions.Modules;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Infrastructure.Messaging
+{
+    internal sealed  class InMemoryMessageBroker : IMessageBroker
+    {
+        private readonly IModuleClient _moduleClient;
+
+        public InMemoryMessageBroker(IModuleClient moduleClient)
+        {
+            _moduleClient = moduleClient;
+        }
+
+        public async Task PublishAsync(params IMessage[] messages)
+        {
+            if (messages is null)
+            {
+                return;
+            }
+
+            messages = messages.Where(m => m is not null).ToArray();
+            if (!messages.Any())
+            {
+                return;
+            }
+
+            var tasks = new List<Task>();
+            foreach (var message in messages)
+            {
+                tasks.Add(_moduleClient.PublishAsync(message));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
