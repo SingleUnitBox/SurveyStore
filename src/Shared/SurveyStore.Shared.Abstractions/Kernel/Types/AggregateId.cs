1636f0feb47f07428411dfc931b2bf425737b64a
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-14 22:37:18 +0000

    add AggregateId

---
+++ src/Shared/SurveyStore.Shared.Abstractions/Kernel/Types/AggregateId.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Abstractions.Kernel.Types
+{
+    public class AggregateId<T> : IEquatable<AggregateId<T>>
+    {
+        public T Value { get; }
+
+        public AggregateId(T value)
+            => Value = value;
+        public bool Equals(AggregateId<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((AggregateId<T>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Shared/SurveyStore.Shared.Abstractions/Kernel/Types/AggregateRoot.cs
+++ src/Shared/SurveyStore.Shared.Abstractions/Kernel/Types/AggregateRoot.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SurveyStore.Shared.Abstractions.Kernel.Types
 {
@@ -6,6 +7,33 @@ namespace SurveyStore.Shared.Abstractions.Kernel.Types
     {
         public T Id { get; protected set; }
         public int Version { get; private set; }
-        public IEnumerable<IDomainEvent> Events { get; set; }
+        public IEnumerable<IDomainEvent> Events => _events;
+
+        private readonly List<IDomainEvent> _events = new();
+        private bool _versionIncremented;
+
+        protected void AddEvent(IDomainEvent @event)
+        {
+            if (!_versionIncremented && _events.Any())
+            {
+                Version++;
+                _versionIncremented = true;
+            }
+
+            _events.Add(@event);
+        }
+
+        protected void IncrementVersion()
+        {
+            if (_versionIncremented)
+            {
+                return;
+            }
+
+            Version++;
+            _versionIncremented = true;
+        }
+
+        public void ClearEvents() => _events.Clear();
     }
 }
