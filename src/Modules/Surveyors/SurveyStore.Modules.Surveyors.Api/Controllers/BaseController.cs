66176154784c58ccd7bd4375e2e7efdf204f67d8
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-16 07:26:39 +0000

    add Surveyors Controllers, modules.json

---
+++ src/Modules/Surveyors/SurveyStore.Modules.Surveyors.Api/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurveyStore.Shared.Infrastructure.Api;
+
+namespace SurveyStore.Modules.Surveyors.Api.Controllers
+{
+    [ProducesDefaultContentType]
+    [ApiController]
+    [Route(SurveyorsModule.BasePath + "/[controller]")]
+    internal class BaseController : ControllerBase
+    {
+        public ActionResult<T> OkOrNotFound<T>(T model)
+        {
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+    }
+}
---
+++ src/Modules/Surveyors/SurveyStore.Modules.Surveyors.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyStore.Modules.Surveyors.Api.Controllers
+{
+
+    internal class HomeController : BaseController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Surveyors API";
+    }
+}
