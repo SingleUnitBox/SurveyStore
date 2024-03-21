1dd5e45c0482183c2e304c2e4a163c7406f17d61
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-21 17:26:36 +0000

    add IDomainEventHandler, IDomainEventDispatcher

---
+++ src/Shared/SurveyStore.Shared.Abstractions/Kernel/IDomainDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Abstractions.Kernel
+{
+    public interface IDomainDispatcher
+    {
+        Task DispatchAsync(params IDomainEvent[] @events);
+    }
+}
---
+++ src/Shared/SurveyStore.Shared.Abstractions/Kernel/IDomainEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Abstractions.Kernel
+{
+    public interface IDomainEventHandler<in TEvent> where TEvent : class, IDomainEvent
+    {
+        Task HandleAsync(TEvent @event);
+    }
+}
