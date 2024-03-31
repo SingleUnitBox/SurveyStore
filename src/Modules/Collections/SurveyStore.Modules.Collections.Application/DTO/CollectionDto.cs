23b01ffe9a13107c926b64dd96272e768bb48dd8
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-03-31 15:17:02 +0200

    add CollectionDto

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/DTO/CollectionDto.cs
@@ -0,0 +1,13 @@
+﻿using SurveyStore.Shared.Abstractions.Kernel.Types;
+
+namespace SurveyStore.Modules.Collections.Application.DTO
+{
+    public class CollectionDto
+	{
+		public CollectionDto()
+		{ 
+			public AggregateId Id { get; set; }
+		}
+	}
+}
+
