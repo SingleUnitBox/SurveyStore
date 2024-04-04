f69c41fe82d876e629ab95cd6b780bc9867f42ae
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-04 21:32:15 +0100

    add BrowseAvailableEquipment

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/Queries/BrowseAvailableEquipment.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using SurveyStore.Modules.Collections.Application.DTO;
+using SurveyStore.Shared.Abstractions.Queries;
+
+namespace SurveyStore.Modules.Collections.Application.Queries
+{
+    public record BrowseAvailableEquipment : IQuery<IEnumerable<SurveyEquipmentDto>>;
+}
