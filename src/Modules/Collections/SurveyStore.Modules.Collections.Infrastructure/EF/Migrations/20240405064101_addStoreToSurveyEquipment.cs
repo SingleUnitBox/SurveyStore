081e429b2983ead05c5feafcd2313c573cfbad9d
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-05 21:39:07 +0100

    edit SurveyEquipment, add Migration

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Infrastructure/EF/Migrations/20240405064101_addStoreToSurveyEquipment.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SurveyStore.Modules.Collections.Infrastructure.EF.Migrations
+{
+    public partial class addStoreToSurveyEquipment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SurveyEquipment_StoreId",
+                schema: "collections",
+                table: "SurveyEquipment",
+                column: "StoreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SurveyEquipment_Stores_StoreId",
+                schema: "collections",
+                table: "SurveyEquipment",
+                column: "StoreId",
+                principalSchema: "collections",
+                principalTable: "Stores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SurveyEquipment_Stores_StoreId",
+                schema: "collections",
+                table: "SurveyEquipment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SurveyEquipment_StoreId",
+                schema: "collections",
+                table: "SurveyEquipment");
+        }
+    }
+}
---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Infrastructure/EF/Queries/Handlers/BrowseAvailableEquipmentHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyStore.Modules.Collections.Application.DTO;
+using SurveyStore.Modules.Collections.Application.Queries;
+using SurveyStore.Modules.Collections.Core.Entities;
+using SurveyStore.Shared.Abstractions.Queries;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SurveyStore.Modules.Collections.Application.Mappings;
+
+namespace SurveyStore.Modules.Collections.Infrastructure.EF.Queries.Handlers
+{
+    public class BrowseAvailableEquipmentHandler : IQueryHandler<BrowseAvailableEquipment, IEnumerable<SurveyEquipmentDto>>
+    {
+        private readonly DbSet<SurveyEquipment> _surveyEquipment;
+
+        public BrowseAvailableEquipmentHandler(CollectionsDbContext dbContext)
+        {
+            _surveyEquipment = dbContext.SurveyEquipment;
+        }
+
+        public async Task<IEnumerable<SurveyEquipmentDto>> HandleAsync(BrowseAvailableEquipment query)
+            => await _surveyEquipment
+                .AsNoTracking()
+                .Include(s => s.Store)
+                .Where(s => s.Store.Id != null)
+                .Select(s => s.AsDto())
+                .ToListAsync();
+    }
+}
