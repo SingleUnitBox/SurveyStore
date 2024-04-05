5d32566c1da319f39081b5b49651615bcf7ec150
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-05 22:53:44 +0100

    add AssignDetails, AssignDetailsHandler

---
+++ src/Modules/Surveyors/SurveyStore.Modules.Surveyors.Core/Commands/AssignDetails.cs
@@ -0,0 +1,6 @@
+﻿using SurveyStore.Shared.Abstractions.Commands;
+
+namespace SurveyStore.Modules.Surveyors.Core.Commands
+{
+    public record AssignDetails(string Email, string FirstName, string Surname, string Position) : ICommand;
+}
---
+++ src/Modules/Surveyors/SurveyStore.Modules.Surveyors.Core/Commands/Handlers/AssignDetailsHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using SurveyStore.Modules.Surveyors.Core.Events;
+using SurveyStore.Modules.Surveyors.Core.Exceptions;
+using SurveyStore.Modules.Surveyors.Core.Repositories;
+using SurveyStore.Shared.Abstractions.Commands;
+using SurveyStore.Shared.Abstractions.Messaging;
+
+namespace SurveyStore.Modules.Surveyors.Core.Commands.Handlers
+{
+    public class AssignDetailsHandler : ICommandHandler<AssignDetails>
+    {
+        private readonly ISurveyorRepository _surveyorRepository;
+        private readonly IMessageBroker _messageBroker;
+
+        public AssignDetailsHandler(ISurveyorRepository surveyorRepository,
+            IMessageBroker messageBroker)
+        {
+            _surveyorRepository = surveyorRepository;
+            _messageBroker = messageBroker;
+        }
+
+        public async Task HandleAsync(AssignDetails command)
+        {
+            var surveyor = await _surveyorRepository.GetByEmailAsync(command.Email);
+            if (surveyor is null)
+            {
+                throw new SurveyorNotFoundException(command.Email);
+            }
+
+            surveyor.FirstName = command.FirstName;
+            surveyor.Surname = command.Surname;
+            surveyor.Position = command.Position ?? "surveyor";
+
+            await _surveyorRepository.UpdateAsync(surveyor);
+            await _messageBroker.PublishAsync(
+                new SurveyorCreated(surveyor.Id, surveyor.FirstName, surveyor.Surname));
+        }
+    }
+}
