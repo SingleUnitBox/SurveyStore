cb71250911dbdbd5890b4bd409fe5094875f6568
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-07 12:03:18 +0100

    add ModuleRequestRegistration

---
+++ src/Shared/SurveyStore.Shared.Infrastructure/Modules/ModuleRequestRegistration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Shared.Infrastructure.Modules
+{
+    public sealed class ModuleRequestRegistration
+    {
+        public Type ReceiverType { get; }
+        public Type ResponseType { get; }
+        public Func<object, Task<object>> Action { get; }
+    }
+}
