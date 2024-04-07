63bf99cb4995430070bd34731cf9aba6710e4049
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-07 23:09:25 +0100

    add ISurveyorsApiClient, DTO

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/Clients/Surveyors/ISurveyorsApiClient.cs
@@ -0,0 +1,11 @@
+﻿using SurveyStore.Modules.Collections.Application.Clients.Surveyors.DTO;
+using System;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Modules.Collections.Application.Clients.Surveyors
+{
+    public interface ISurveyorsApiClient
+    {
+        Task<SurveyorDto> GetSurveyorAsync(Guid id);
+    }
+}
