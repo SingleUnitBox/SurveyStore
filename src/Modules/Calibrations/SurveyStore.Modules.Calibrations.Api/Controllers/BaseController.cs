b8222cb5dfe8cf96bc2442b47e49e5138ed4c10d
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-15 19:31:34 +0100

    add HomeController, BaseController, Calibrations.http

---
+++ src/Modules/Calibrations/SurveyStore.Modules.Calibrations.Api/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyStore.Modules.Calibrations.Api.Controllers
+{
+    [ApiController]
+    [Route(CalibrationsModule.BasePath + "/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        public ActionResult<T> OkOrNotFound<T>(T model)
+        {
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+    }
+}
---
+++ src/Modules/Calibrations/SurveyStore.Modules.Calibrations.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyStore.Modules.Calibrations.Api.Controllers
+{
+    [Route(CalibrationsModule.BasePath)]
+    public class HomeController : BaseController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Calibrations API";
+    }
+}
