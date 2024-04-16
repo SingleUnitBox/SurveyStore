1ac36ba46151f4b2903110a8c31f95a41a39c5bc
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-16 18:32:14 +0100

    add ICalibrationsApiClient, CalibrationDto

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/Clients/Calibrations/DTO/CalibrationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SurveyStore.Modules.Collections.Application.Clients.Calibrations.DTO
+{
+    public class CalibrationDto
+    {
+        public Guid SurveyEquipmentId { get; set; }
+        public Guid SurveyEquipment { get; set; }
+        public DateTime CalibrationDueDate { get; set; }
+        public TimeSpan CalibrationInterval { get; set; }
+        public string CertificateNumber { get; set; }
+        public string CalibrationStatus { get; set; }
+    }
+}
