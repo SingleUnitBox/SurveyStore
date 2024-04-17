3bc76c596b3a4284a4838167684eb3175741ff4b
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-17 18:22:25 +0100

    edit CalibrationConfigurationToHaveCalibrationStatusAsString

---
+++ src/Modules/Calibrations/SurveyStore.Modules.Calibrations.Infrastructure/EF/Migrations/20240417171816_EditCalibrationConfigurationToHaveCalibrationStatusAsString.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SurveyStore.Modules.Calibrations.Infrastructure.EF.Migrations
+{
+    public partial class EditCalibrationConfigurationToHaveCalibrationStatusAsString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CalibrationStatus",
+                schema: "calibrations",
+                table: "Calibrations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CalibrationStatus",
+                schema: "calibrations",
+                table: "Calibrations",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
