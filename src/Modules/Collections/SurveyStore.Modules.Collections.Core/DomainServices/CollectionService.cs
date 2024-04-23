7a358e97bd9edd21c0b5d38d3c301aa2fd83aad2
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-23 18:48:36 +0100

    add ICollectionService, CollectionService

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Core/DomainServices/CollectionService.cs
@@ -0,0 +1,15 @@
+﻿using SurveyStore.Modules.Collections.Core.Entities;
+using SurveyStore.Shared.Abstractions.Kernel.Types;
+using System.Collections.Generic;
+
+namespace SurveyStore.Modules.Collections.Core.DomainServices
+{
+    internal class CollectionService : ICollectionService
+    {
+        public void Collect(IEnumerable<Collection> allCollections, SurveyorId surveyorId, Collection toBeCollected)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+ 
---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Core/DomainServices/ICollectionService.cs
@@ -0,0 +1,12 @@
+﻿using SurveyStore.Modules.Collections.Core.Entities;
+using SurveyStore.Shared.Abstractions.Kernel.Types;
+using System.Collections.Generic;
+
+namespace SurveyStore.Modules.Collections.Core.DomainServices
+{
+    internal interface ICollectionService
+    {
+        void Collect(IEnumerable<Collection> allCollections, SurveyorId surveyorId, Collection toBeCollected);
+    }
+}
+ 
