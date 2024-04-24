58ebabb7a4f913a01c9c6d0a758c7e2a1ea576a6
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-04-24 06:41:33 +0100

    edit CollectionModule to fulfill new business requirments

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Core/Exceptions/CannotCollectSurveyEquipmentException.cs
@@ -0,0 +1,14 @@
+﻿using SurveyStore.Shared.Abstractions.Exceptions;
+
+namespace SurveyStore.Modules.Collections.Core.Exceptions
+{
+    public class CannotCollectSurveyEquipmentException : SurveyStoreException
+    {
+        public string Type { get; }
+        public CannotCollectSurveyEquipmentException(string type)
+            : base($"Cannot collect survey equipment of type '{type}'.")
+        {
+            Type = type;
+        }
+    }
+}
