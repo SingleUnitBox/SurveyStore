1e33dde4ae0d9c2923b3874de0cfdaa96830bb04
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-06 21:12:34 +0100

    add IKitRepository

---
+++ src/Modules/Equipment/SurveyStore.Modules.Equipment.Domain/Kit/Repositories/IKitRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Modules.Equipment.Domain.Kit.Repositories
+{
+    public interface IKitRepository
+    {
+        Task AddAsync(Entities.Kit kit);
+        Task UpdateAsync(Entities.Kit kit);
+        Task<Entities.Kit> GetAsyncById(Guid id);
+        Task<IReadOnlyCollection<Entities.Kit>> BrowseAsync();
+    }
+}
