e411ea83ccd7b587891c86a380d0c9421c9e69e8
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-08 07:58:38 +0100

    add Collectios.Domain.Kit, edit StoreAssigned DomainEvent

--- src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/DomainEvents/StoreAssigned.cs
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/DomainEvents/StoreAssigned.cs
@@ -4,5 +4,24 @@ using SurveyStore.Shared.Abstractions.Kernel.Types;
 
 namespace SurveyStore.Modules.Collections.Domain.Collections.DomainEvents
 {
-    public record StoreAssigned(SurveyEquipment SurveyEquipment, StoreId StoreId) : IDomainEvent;
+    //public record StoreAssigned(SurveyEquipment SurveyEquipment, StoreId StoreId) : IDomainEvent;
+    //public record StoreAssigned(Kit Kit, StoreId storeId) : IDomainEvent;
+    public class StoreAssigned : IDomainEvent
+    {
+        public SurveyEquipment SurveyEquipment { get; init; }
+        public Kit Kit { get; init; }
+        public StoreId StoreId { get; init; }
+
+        public StoreAssigned(SurveyEquipment surveyEquipment, StoreId storeId)
+        {
+            SurveyEquipment = surveyEquipment;
+            StoreId = storeId;
+        }
+
+        public StoreAssigned(Kit kit, StoreId storeId)
+        {
+            Kit = kit;
+            StoreId = storeId;
+        }
+    }
 }
---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Entities/Kit.cs
@@ -0,0 +1,40 @@
+﻿using SurveyStore.Modules.Collections.Domain.Collections.DomainEvents;
+using SurveyStore.Shared.Abstractions.Kernel.Types;
+using System;
+
+namespace SurveyStore.Modules.Collections.Domain.Collections.Entities
+{
+    public class Kit : AggregateRoot
+    {
+        public Store Store { get; private set; }
+        public StoreId StoreId { get; private set; }
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public string SerialNumber { get; private set; }
+        public string Type { get; private set; }
+
+        protected Kit(AggregateId id, string brand, string model, string serialNumber, string type)
+        {
+            Id = id;
+            Brand = brand;
+            Model = model;
+            SerialNumber = serialNumber;
+            Type = type;
+        }
+
+        public static Kit Create(Guid id, string brand, string model, string serialNumber, string type)
+        {
+            var kit = new Kit(id, brand, model, serialNumber, type);
+            kit.ClearEvents();
+            kit.Version = 0;
+
+            return kit;
+        }
+
+        public void AssignStore(StoreId storeId)
+        {
+            StoreId = storeId;
+            AddEvent(new StoreAssigned(this, storeId));
+        }
+    }
+}
