8f8b4585216f4db035019f1076c32cf16de3cdae
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-09 22:18:17 +0100

    add EventHandlerDecorator

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/Decorators/Events/EventHandlerDecorator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using SurveyStore.Shared.Abstractions.Events;
+using SurveyStore.Shared.Infrastructure.Postgres.Decorators;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Modules.Collections.Application.Decorators.Events
+{
+    [Decorator]
+    internal sealed class EventHandlerDecorator<TEvent> : IEventHandler<TEvent>
+        where TEvent : class, IEvent
+    {
+        private readonly IEventHandler<TEvent> _eventHandler;
+        private readonly ILogger<EventHandlerDecorator<TEvent>> _logger;
+
+        public EventHandlerDecorator(IEventHandler<TEvent> eventHandler,
+            ILogger<EventHandlerDecorator<TEvent>> logger)
+        {
+            _eventHandler = eventHandler;
+            _logger = logger;
+        }
+
+        public async Task HandleAsync(TEvent @event)
+        {
+            await _eventHandler.HandleAsync(@event);
+            _logger.LogInformation($"Finishing processing event '{@event.GetType().Name}'.");
+        }
+    }
+}
