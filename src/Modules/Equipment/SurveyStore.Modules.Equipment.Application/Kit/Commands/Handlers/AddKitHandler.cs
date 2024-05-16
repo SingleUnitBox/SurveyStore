5f888898ec96c2fbf6d8bf0560f33cda6f51236e
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-16 20:07:33 +0100

    edit AddKitHandler

--- src/Modules/Equipment/SurveyStore.Modules.Equipment.Application/Kit/Commands/Handlers/AddKitHandler.cs
+++ src/Modules/Equipment/SurveyStore.Modules.Equipment.Application/Kit/Commands/Handlers/AddKitHandler.cs
@@ -1,6 +1,11 @@
-﻿using SurveyStore.Modules.Equipment.Application.Kit.Exceptions;
+﻿using SurveyStore.Modules.Equipment.Application.Kit.Events;
+using SurveyStore.Modules.Equipment.Application.Kit.Exceptions;
+using SurveyStore.Modules.Equipment.Domain.Kit.Entities;
+using SurveyStore.Modules.Equipment.Domain.Kit.Repositories;
 using SurveyStore.Shared.Abstractions.Commands;
+using SurveyStore.Shared.Abstractions.Messaging;
 using SurveyStore.Shared.Abstractions.Types;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +13,16 @@ namespace SurveyStore.Modules.Equipment.Application.Kit.Commands.Handlers
 {
     public class AddKitHandler : ICommandHandler<AddKit>
     {
+        private readonly IKitRepository _kitRepository;
+        private readonly IMessageBroker _messageBroker;
+
+        public AddKitHandler(IKitRepository kitRepository,
+            IMessageBroker messageBroker)
+        {
+            _kitRepository = kitRepository;
+            _messageBroker = messageBroker;
+        }
+
         public async Task HandleAsync(AddKit command)
         {
             var kitTypes = typeof(KitTypes).GetFields().Select(t => t.Name);
@@ -15,7 +30,24 @@ namespace SurveyStore.Modules.Equipment.Application.Kit.Commands.Handlers
             {
                 throw new InvalidKitTypeException(command.KitType);
             }
-            return;
+
+            var kit = await _kitRepository.GetBySerialNumberAsync(command.SerialNumber);
+            if (kit is not null)
+            {
+                throw new KitAlreadyExistsException(command.KitType, command.SerialNumber);
+            }
+
+            kit = command.KitType switch
+            {
+                KitTypes.Tripod => Tripod.Create(command.Id, command.Brand, command.Model, command.SerialNumber, command.PurchasedAt),
+                KitTypes.TraversePrism => TraversePrism.Create(command.Id, command.Brand, command.Model, command.SerialNumber, command.PurchasedAt),
+                KitTypes.DetailPole => DetailPole.Create(command.Id, command.Brand, command.Model, command.SerialNumber, command.PurchasedAt),
+                KitTypes.GNSSPole => GNSSPole.Create(command.Id, command.Brand, command.Model, command.SerialNumber, command.PurchasedAt),
+                _ => throw new ArgumentNullException()
+            };
+
+            await _kitRepository.AddAsync(kit);
+            await _messageBroker.PublishAsync(new KitCreated(kit.Id));
         }
     }
 }
