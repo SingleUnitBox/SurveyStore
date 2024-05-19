68e4b530aac8cafc5e7d7ac18f43c13840c50af3
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-19 22:12:55 +0100

    edit UnitOfWorkTypeRegistry

--- src/Shared/SurveyStore.Shared.Infrastructure/Postgres/UnitOfWorkTypeRegistry.cs
+++ src/Shared/SurveyStore.Shared.Infrastructure/Postgres/UnitOfWorkTypeRegistry.cs
@@ -6,8 +6,10 @@ namespace SurveyStore.Shared.Infrastructure.Postgres
     internal class UnitOfWorkTypeRegistry
     {
         private readonly Dictionary<string, Type> _types = new();
-        public void Register<T>() where T : IUnitOfWork => _types[GetKey<T>()] = typeof(T);
-        public Type Resolve<T>() => _types.TryGetValue(GetKey<T>(), out var type) ? type : null;
+        //public void Register<T>() where T : IUnitOfWork => _types.TryAdd("key", typeof(T));
+        public void Register<T>() where T : IUnitOfWork => _types["key"] = typeof(T);
+        public Type Resolve<T>()
+            => _types.TryGetValue(GetKey<>, out var type) ? type : null;
         private static string GetKey<T>() => $"{typeof(T).GetModuleName()}";
     }
 }
