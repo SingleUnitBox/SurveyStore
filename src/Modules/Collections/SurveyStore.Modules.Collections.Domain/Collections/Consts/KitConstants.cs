b580b39d410206ce749f4574e23c7d3c36f24659
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-20 23:13:50 +0100

    add Consts

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Consts/KitConstants.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace SurveyStore.Modules.Collections.Domain.Collections.Consts
+{
+    public class KitConstants
+    {
+        public const int TripodRequiredAmount = 3;
+        public const int PrismRequiredAmount = 2;
+
+        public static readonly string[] LimitedSurveyEquipmentTypes = new[]
+            {
+                "total station",
+                "gnss"
+            };
+    }
+}
