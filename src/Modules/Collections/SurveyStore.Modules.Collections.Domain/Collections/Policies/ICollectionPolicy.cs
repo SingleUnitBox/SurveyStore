cea74af652fcc9e18730d00fd886c32b915bf0f7
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-20 21:15:22 +0100

    add ICollectionPolicy

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Policies/ICollectionPolicy.cs
@@ -0,0 +1,9 @@
+﻿using SurveyStore.Modules.Collections.Domain.Collections.Entities;
+
+namespace SurveyStore.Modules.Collections.Domain.Collections.Policies
+{
+    public interface ICollectionPolicy
+    {
+        bool CanBeReturned(Collection collection, Surveyor surveyor);
+    }
+}
