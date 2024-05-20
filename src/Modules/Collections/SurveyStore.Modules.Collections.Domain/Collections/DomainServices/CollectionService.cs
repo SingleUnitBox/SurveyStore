356789b05fbe7d50322550c5dfc23e8fc28aa86e
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-20 23:13:02 +0100

    edit CollectionService

--- src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/DomainServices/CollectionService.cs
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/DomainServices/CollectionService.cs
@@ -1,4 +1,5 @@
-﻿using SurveyStore.Modules.Collections.Domain.Collections.Entities;
+﻿using SurveyStore.Modules.Collections.Domain.Collections.Consts;
+using SurveyStore.Modules.Collections.Domain.Collections.Entities;
 using SurveyStore.Modules.Collections.Domain.Collections.Exceptions;
 using SurveyStore.Modules.Collections.Domain.Collections.Policies;
 using SurveyStore.Shared.Abstractions.Types;
@@ -10,15 +11,6 @@ namespace SurveyStore.Modules.Collections.Domain.Collections.DomainServices
 {
     internal class CollectionService : ICollectionService
     {
-        private readonly int _tripodRequiredAmount = 3;
-        private readonly int _prismRequiredAmount = 2;
-
-        private readonly string[] limitedSurveyEquipmentTypes = new[]
-            {
-                "total station",
-                "gnss"
-            };
-
         private readonly IKitCollectionPolicy _kitCollectionPolicy;
 
         public CollectionService(IKitCollectionPolicy kitCollectionPolicy)
@@ -31,7 +23,7 @@ namespace SurveyStore.Modules.Collections.Domain.Collections.DomainServices
             var surveyEquipmentTypes = openCollections.Select(c => c.SurveyEquipment.Type);
             foreach (var surveyEquipmentType in surveyEquipmentTypes)
             {
-                if (limitedSurveyEquipmentTypes.Contains(surveyEquipmentType)
+                if (KitConstants.LimitedSurveyEquipmentTypes.Contains(surveyEquipmentType)
                     && toBeCollected.SurveyEquipment.Type == surveyEquipmentType)
                 {
                     throw new CannotCollectSurveyEquipmentException(surveyEquipmentType);
@@ -45,29 +37,29 @@ namespace SurveyStore.Modules.Collections.Domain.Collections.DomainServices
             Surveyor surveyor, Collection toBeCollected, DateTime now)
         {
             var (isEnough, actualAmount) = _kitCollectionPolicy
-                .IsEnoughKit(freeKitCollections, KitTypes.Tripod.ToString(), _tripodRequiredAmount);
+                .IsEnoughKit(freeKitCollections, KitTypes.Tripod.ToString(), KitConstants.TripodRequiredAmount);
             if (!isEnough)
             {
-                throw new NotEnoughKitAvailableToFormSetException(KitTypes.Tripod, _tripodRequiredAmount, actualAmount);
+                throw new NotEnoughKitAvailableToFormSetException(KitTypes.Tripod, KitConstants.TripodRequiredAmount, actualAmount);
             }
 
             (isEnough, actualAmount) = _kitCollectionPolicy
-                .IsEnoughKit(freeKitCollections, KitTypes.TraversePrism.ToString(), _prismRequiredAmount);
+                .IsEnoughKit(freeKitCollections, KitTypes.TraversePrism.ToString(), KitConstants.PrismRequiredAmount);
             if (!isEnough)
             {
-                throw new NotEnoughKitAvailableToFormSetException(KitTypes.TraversePrism, _prismRequiredAmount, actualAmount);
+                throw new NotEnoughKitAvailableToFormSetException(KitTypes.TraversePrism, KitConstants.PrismRequiredAmount, actualAmount);
             }
 
             var collectionStoreId = toBeCollected.CollectionStoreId;
             var tripods = _kitCollectionPolicy.KitToBeCollected(freeKitCollections, KitTypes.Tripod.ToString(),
-                collectionStoreId, _tripodRequiredAmount);
+                collectionStoreId, KitConstants.TripodRequiredAmount);
             foreach (var tripod in tripods)
             {
                 tripod.Collect(surveyor, now);
             }
 
             var prisms = _kitCollectionPolicy.KitToBeCollected(freeKitCollections, KitTypes.TraversePrism.ToString(),
-                collectionStoreId, _prismRequiredAmount);
+                collectionStoreId, KitConstants.PrismRequiredAmount);
             foreach (var prism in prisms)
             {
                 prism.Collect(surveyor, now);
