fe68b1b44ab8a46968ea4d632cf4dd840b260105
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-20 20:47:17 +0100

    add ReturnTraverseSetHandler

---
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/Commands/Handlers/ReturnTraverseSetHandler.cs
@@ -0,0 +1,36 @@
+﻿using SurveyStore.Modules.Collections.Domain.Collections.Repositories;
+using SurveyStore.Shared.Abstractions.Commands;
+using System.Threading.Tasks;
+
+namespace SurveyStore.Modules.Collections.Application.Commands.Handlers
+{
+    public class ReturnTraverseSetHandler : ICommandHandler<ReturnTraverseSet>
+    {
+        private readonly ISurveyEquipmentRepository _equipmentRepository;
+        private readonly IKitRepository _kitRepository;
+        private readonly IStoreRepository _storeRepository;
+        private readonly ISurveyorRepository _surveyorRepository;
+        private readonly ICollectionRepository _collectionRepository;
+        private readonly IKitCollectionRepository _kitCollectionRepository;
+
+        public ReturnTraverseSetHandler(ISurveyEquipmentRepository equipmentRepository,
+            IKitRepository kitRepository,
+            IStoreRepository storeRepository,
+            ISurveyorRepository surveyorRepository,
+            ICollectionRepository collectionRepository,
+            IKitCollectionRepository kitCollectionRepository)
+        {
+            _equipmentRepository = equipmentRepository;
+            _kitRepository = kitRepository;
+            _storeRepository = storeRepository;
+            _surveyorRepository = surveyorRepository;
+            _collectionRepository = collectionRepository;
+            _kitCollectionRepository = kitCollectionRepository;
+        }
+
+        public Task HandleAsync(ReturnTraverseSet command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
