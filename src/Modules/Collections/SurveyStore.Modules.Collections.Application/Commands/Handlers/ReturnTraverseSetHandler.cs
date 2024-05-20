2fae09e3793f69ae52664eb0a7f11964f7422449
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-20 21:01:06 +0100

    edit ReturnTraverseSetHandler

--- src/Modules/Collections/SurveyStore.Modules.Collections.Application/Commands/Handlers/ReturnTraverseSetHandler.cs
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Application/Commands/Handlers/ReturnTraverseSetHandler.cs
@@ -1,4 +1,5 @@
-﻿using SurveyStore.Modules.Collections.Domain.Collections.Repositories;
+﻿using SurveyStore.Modules.Collections.Application.Exceptions;
+using SurveyStore.Modules.Collections.Domain.Collections.Repositories;
 using SurveyStore.Shared.Abstractions.Commands;
 using System.Threading.Tasks;
 
@@ -6,21 +7,21 @@ namespace SurveyStore.Modules.Collections.Application.Commands.Handlers
 {
     public class ReturnTraverseSetHandler : ICommandHandler<ReturnTraverseSet>
     {
-        private readonly ISurveyEquipmentRepository _equipmentRepository;
+        private readonly ISurveyEquipmentRepository _surveyEquipmentRepository;
         private readonly IKitRepository _kitRepository;
         private readonly IStoreRepository _storeRepository;
         private readonly ISurveyorRepository _surveyorRepository;
         private readonly ICollectionRepository _collectionRepository;
         private readonly IKitCollectionRepository _kitCollectionRepository;
 
-        public ReturnTraverseSetHandler(ISurveyEquipmentRepository equipmentRepository,
+        public ReturnTraverseSetHandler(ISurveyEquipmentRepository surveyEquipmentRepository,
             IKitRepository kitRepository,
             IStoreRepository storeRepository,
             ISurveyorRepository surveyorRepository,
             ICollectionRepository collectionRepository,
             IKitCollectionRepository kitCollectionRepository)
         {
-            _equipmentRepository = equipmentRepository;
+            _surveyEquipmentRepository = surveyEquipmentRepository;
             _kitRepository = kitRepository;
             _storeRepository = storeRepository;
             _surveyorRepository = surveyorRepository;
@@ -28,9 +29,38 @@ namespace SurveyStore.Modules.Collections.Application.Commands.Handlers
             _kitCollectionRepository = kitCollectionRepository;
         }
 
-        public Task HandleAsync(ReturnTraverseSet command)
+        public async Task HandleAsync(ReturnTraverseSet command)
         {
-            throw new System.NotImplementedException();
+            var surveyor = await _surveyorRepository.GetAsync(command.SurveyorId);
+            if (surveyor is null)
+            {
+                throw new SurveyorNotFoundException(command.SurveyorId);
+            }
+
+            var store = await _storeRepository.GetByIdAsync(command.ReturnStoreId);
+            if (store is null)
+            {
+                throw new StoreNotFoundException(command.ReturnStoreId);
+            }
+
+            var surveyEquipment = await _surveyEquipmentRepository.GetByIdAsync(command.SurveyEquipmentId);
+            if (surveyEquipment is null)
+            {
+                throw new SurveyEquipmentNotFoundException(command.SurveyEquipmentId);
+            }
+
+            var collection = await _collectionRepository.GetOpenBySurveyEquipmentAsync(command.SurveyEquipmentId);
+            if (collection is null)
+            {
+                throw new OpenCollectionNotFoundException(command.SurveyEquipmentId);
+            }
+
+            if (collection.Surveyor.Id != surveyor.Id)
+            {
+                throw new ReturningOtherCollectionException(collection.Id, surveyor.Id);
+            }
+
+            collection.
         }
     }
 }
