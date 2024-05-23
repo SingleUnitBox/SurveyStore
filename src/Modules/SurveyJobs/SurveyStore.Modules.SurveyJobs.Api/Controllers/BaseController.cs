9b80817652f8657acbf7a720a4bcacf91822879b
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-23 23:21:49 +0100

    add BaseController, HomeController

---
+++ src/Modules/SurveyJobs/SurveyStore.Modules.SurveyJobs.Api/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyStore.Modules.SurveyJobs.Api.Controllers
+{
+    [ApiController]
+    [Route(SurveyJobsModule.BasePath + "[/controller]")]
+    public class BaseController : ControllerBase
+    {
+        public ActionResult<T> OkOrNotFound<T>(T model)
+        {
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+    }
+}
---
+++ src/Modules/SurveyJobs/SurveyStore.Modules.SurveyJobs.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyStore.Modules.SurveyJobs.Api.Controllers
+{
+    [Route(SurveyJobsModule.BasePath)]
+    public class HomeController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "SurveyJobs API";
+    }
+}
