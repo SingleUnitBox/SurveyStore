33e0083aa71059edfe712b98de986b09c0170192
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-23 22:40:26 +0100

    add ModuleReferences, SurveyJobsModule

---
+++ src/Modules/SurveyJobs/SurveyStore.Modules.SurveyJobs.Api/SurveyJobsModule.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SurveyStore.Shared.Abstractions.Modules;
+
+namespace SurveyStore.Modules.SurveyJobs.Api
+{
+    public class SurveyJobsModule : IModule
+    {
+        public string Name => throw new System.NotImplementedException();
+
+        public string Path => throw new System.NotImplementedException();
+
+        public void Register(IServiceCollection services)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Use(IApplicationBuilder app)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
