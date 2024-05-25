3cc19561989e2382e29dbc8b0e11606ac356a99f
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-05-25 22:05:07 +0100

    add DocumentConfiguration, edit SurveyorConfiguration

--- src/Modules/SurveyJobs/SurveyStore.Modules.SurveyJobs.Infrastructure/EF/Configuration/SurveyorConfiguration.cs
+++ src/Modules/SurveyJobs/SurveyStore.Modules.SurveyJobs.Infrastructure/EF/Configuration/SurveyorConfiguration.cs
@@ -1,6 +1,18 @@
-﻿namespace SurveyStore.Modules.SurveyJobs.Infrastructure.EF.Configuration
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SurveyStore.Modules.SurveyJobs.Domain.Entities;
+
+namespace SurveyStore.Modules.SurveyJobs.Infrastructure.EF.Configuration
 {
-    public class SurveyorConfiguration
+    public class SurveyorConfiguration : IEntityTypeConfiguration<Surveyor>
     {
+        public void Configure(EntityTypeBuilder<Surveyor> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Id)
+                .HasConversion(s => s.Value, s => new(s));
+            builder.Property(s => s.Version)
+                .IsConcurrencyToken();
+        }
     }
 }
