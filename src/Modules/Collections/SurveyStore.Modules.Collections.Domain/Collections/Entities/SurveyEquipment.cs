0ecab7113b37dec26d1dd8cd3e1e7f935699fed3
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-06-19 21:12:17 +0100

    edit SurveyEquipment

--- src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Entities/SurveyEquipment.cs
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Entities/SurveyEquipment.cs
@@ -1,11 +1,11 @@
-﻿using SurveyStore.Modules.Collections.Domain.Collections.DomainEvents;
-using SurveyStore.Shared.Abstractions.Kernel.Types;
+﻿using SurveyStore.Shared.Abstractions.Kernel.Types;
 using System;
 
 namespace SurveyStore.Modules.Collections.Domain.Collections.Entities
 {
-    public class SurveyEquipment : AggregateRoot
+    public class SurveyEquipment
     {
+        public SurveyEquipmentId Id { get; private set; }
         public StoreId StoreId { get; private set; }
         public Store Store { get; private set; }
         public string SerialNumber { get; private set; }
@@ -14,7 +14,7 @@ namespace SurveyStore.Modules.Collections.Domain.Collections.Entities
         public string Type { get; private set; }
         public bool IsFree => StoreId is null;
 
-        public SurveyEquipment(AggregateId id, string serialNumber, string brand, string model, string type)
+        private SurveyEquipment(SurveyEquipmentId id, string serialNumber, string brand, string model, string type)
         {
             Id = id;
             SerialNumber = serialNumber;
@@ -23,26 +23,23 @@ namespace SurveyStore.Modules.Collections.Domain.Collections.Entities
             Type = type;
         }
 
-        public static SurveyEquipment Create(Guid id, string serialNumber, string brand, string model, string type)
-        {
-            var equipment = new SurveyEquipment(id, serialNumber, brand, model, type);
-
-            equipment.ClearEvents();
-            equipment.Version = 0;
-
-            return equipment;
-        }
-
         public void AssignStore(StoreId storeId)
         {
             StoreId = storeId;
-            AddEvent(new StoreAssigned(this, storeId));
+            //AddEvent(new StoreAssigned(this, storeId));
         }
 
         public void UnassignStore()
         {
             StoreId = null;
-            IncrementVersion();
+            //IncrementVersion();
+        }
+
+        public static SurveyEquipment Create(Guid id, string serialNumber, string brand, string model, string type)
+        {
+            var equipment = new SurveyEquipment(id, serialNumber, brand, model, type);
+
+            return equipment;
         }
     }
 }
