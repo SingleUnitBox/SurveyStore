0cde2a94a380b93649c75aa3e3f33a37d572ebea
SingleUnitBox <cezaryzukowski90@gmail.com>
2024-06-25 21:29:07 +0100

    add IsCompletedCollection

--- src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Specifications/IsCompletedCollection.cs
+++ src/Modules/Collections/SurveyStore.Modules.Collections.Domain/Collections/Specifications/IsCompletedCollection.cs
@@ -5,20 +5,18 @@ using System.Linq.Expressions;
 
 namespace SurveyStore.Modules.Collections.Domain.Collections.Specifications
 {
-    public class IsFreeCollection : Specification<Collection>
+    public class IsCompletedCollection : Specification<Collection>
     {
         private readonly Guid _surveyEquipmentId;
-
-        public IsFreeCollection(Guid surveyEquipmentId)
+        public IsCompletedCollection(Guid surveyEquipmentId)
         {
             _surveyEquipmentId = surveyEquipmentId;
         }
-
         protected override Expression<Func<Collection, bool>> AsPredicateExpression()
         {
-            return collection => collection.SurveyEquipmentId == _surveyEquipmentId
-                                && collection.CollectedAt == null
-                                && collection.ReturnStoreId == null;
+            return collection => collection.SurveyEquipmentId.Value == _surveyEquipmentId
+                                && collection.CollectedAt != null
+                                && collection.ReturnStoreId != null;
         }
     }
 }
